c2ec58707821f4fa00b27b24575986f0d13a4ab6
arhernav <53274573+arhernav@users.noreply.github.com>
2023-12-06 02:26:03 -0600

    Update agrupacionTerrorismo.R

--- agrupacionTerrorismo.R
+++ agrupacionTerrorismo.R
@@ -1,2 +1,35 @@
 # Minado de datos - agrupación 
 
+data <- read.csv("afgData.csv", header=T, na.strings=c("","NA"))
+
+# Instalar los paquetes si no están instalados previamente
+install.packages(c("readxl", "tibble", "tidyverse", "cluster", "factoextra", "NbClust", "tidyr", "textshape"))
+
+# Cargar los paquetes
+library(readxl)
+library(tibble)
+library(tidyverse)
+library(cluster)
+library(factoextra)
+library(NbClust)
+library(tidyr)
+library(textshape)
+library(dplyr)
+
+# Seleccionar solo las columnas numéricas
+data_numeric <- data %>% select_if(is.numeric)
+
+# Normalizar 
+data_scaled <- scale(data_numeric)
+
+# Mostrar las cabeceras del dataframe normalizado
+head(data_scaled)
+
+#estimar el número de clústers
+fviz_nbclust(data_scaled, kmeans, method = "silhouette")
+
+#con el resultado anterior observamos que el mejor candidato es 4
+#por lo tanto, calculamos a 4 clústers
+k4 <- kmeans(data_scaled, centers = 4, nstart = 25)
+k4
+str(k4)
