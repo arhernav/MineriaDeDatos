edd85000fcf53485ca6b314ca4e56c213167c0d3
arhernav <53274573+arhernav@users.noreply.github.com>
2023-12-08 02:47:15 -0600

    Añadida red neuronal

--- clasificacionTerrorismo.R
+++ clasificacionTerrorismo.R
@@ -69,3 +69,48 @@ print("Predicción de targsubtype = 21 y targtype = 2")
 predict(tre, newdata=new)
 
 ## Red neuronal
+data <- read.csv("~/Documents/Mining/afgData.csv")
+data$crit3 <- as.factor(data$crit3)
+library(caTools)
+set.seed(123)
+
+spl <- sample.split(data$X, SplitRatio = .75)
+training = subset(data, spl == TRUE)
+testing = subset(data, spl == FALSE)
+
+cols <- names(data)
+cols <- cols[-which(names(data) %in% c("X","eventid", "iyear", "imonth", "iday", 
+                                       "provstate", "city", "alternative_txt",
+                                       "attacktype1_txt", "targtype1_txt", 
+                                       "targsubtype1_txt", "corp1", "target1", 
+                                       "natlty1_txt", "gname", "weaptype1_txt",
+                                       "extended", "specificity", "vicinity", 
+                                       "alternative", "multiple", 
+                                       "natlty1", "guncertain1", "nwoundus",
+                                       "nkill", "nwound", "targsubtype1", 
+                                       "crit1", "crit2", "crit3", "success", 
+                                       "suicide", "attacktype1", "targtype1"))]
+
+form <- paste(cols,collapse=' + ')
+form <- paste('crit3 ~',form)
+form <- as.formula(form)
+
+library(neuralnet)
+
+sigmoid = function(x) {1/(1 + exp(-x))}
+red <- neuralnet(form,training,hidden=c(2,2),linear.output=FALSE)
+
+out <- cbind(red$covariate, + red$net.result[[1]])
+
+library(dplyr)
+prediction <- predict(red,testing)
+
+check = as.numeric(testing$crit3) == max.col(prediction)
+accuracy = (sum(check)/nrow(testing))*100
+print(accuracy)
+
+
+
+
+
+
