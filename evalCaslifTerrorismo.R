5b91b98b5af6bb4414cfbeabaa79774c383e7b96
arhernav <53274573+arhernav@users.noreply.github.com>
2023-12-08 11:27:38 -0600

    Update evalCaslifTerrorismo.R

--- evalCaslifTerrorismo.R
+++ evalCaslifTerrorismo.R
@@ -1,1 +1,17 @@
 # Minado de datos - Evaluación de modelos de clasificación
+
+### Evaluación del arbol previamente creado
+mean(doubterr == tre)
+
+confMat <- table(data$doubterr,tre)
+accuracy <- sum(diag(confMat))/sum(confMat)
+
+
+
+### Evaluación de la red previamente creada
+library(dplyr)
+prediction <- predict(red,testing)
+
+check = as.numeric(testing$crit3) == max.col(prediction)
+accuracy = (sum(check)/nrow(testing))*100
+print(accuracy)
